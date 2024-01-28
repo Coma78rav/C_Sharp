83e7907dfce4d393774aab99f7c5ea366957d97c
Andrey Rakov <coma78rav@gmail.com>
2024-01-28 20:04:59 +0300

    Задание 2

--- Program.cs
+++ Program.cs
@@ -6,13 +6,30 @@
 // a = 9, b = -3 => да
 // a = -3, b = 9 => нет
 
-System.Console.WriteLine("Введите первое целое число");
-int num1 = Convert.ToInt32 (Console.ReadLine ());
-System.Console.WriteLine("Введите второе целое число");
-int num2 = Convert.ToInt32 (Console.ReadLine ());
-if (num2 * num2 == num1)
+// System.Console.WriteLine("Введите первое целое число");
+// int num1 = Convert.ToInt32 (Console.ReadLine ());
+// System.Console.WriteLine("Введите второе целое число");
+// int num2 = Convert.ToInt32 (Console.ReadLine ());
+// if (num2 * num2 == num1)
+// {
+//     System.Console.WriteLine("да");
+// }
+// else
+//     System.Console.WriteLine("нет");
+
+// Задание 2. Совместная работа
+// Напишите программу, которая на вход принимает целое число N, 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+System.Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = - num;
+while (i < num)
 {
-    System.Console.WriteLine("да");
+    Console.Write (i + ", ");
+    i = i+1;
 }
-else
-    System.Console.WriteLine("нет");
+Console.WriteLine(i);
--- obj/Debug/net6.0/C_Sharp.AssemblyInfo.cs
+++ obj/Debug/net6.0/C_Sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C_Sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b8125227b0c00b31bbdef0abf9e9943fc4044d")]
 [assembly: System.Reflection.AssemblyProductAttribute("C_Sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C_Sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
